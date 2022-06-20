1e71320b31fb64f9d8179aa873c3cb63263a61ff
Sergio Valadão <sergiovaladao@gmail.com>
2022-06-20 17:54:36 -0300

    ChessException e news class

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,7 +1,5 @@
 package boardgame;
 
-import boardgame.Piece;
-
 public class Board {
 	
 	private int rows;
@@ -18,24 +16,56 @@ public class Board {
 		return rows;
 	}
 
+	/*
 	public void setRow(int rows) {
 		this.rows = rows;
 	}
+	*/
 
 	public int getColumns() {
 		return columns;
 	}
 
+	/*
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
+	*/
 	
 	public Piece piece(int row, int col) {
+		if(!positionExists(row, col)) {
+			throw new BoardException("Posição não existe no tabuleiro");
+		}
 		return pieces[row][col];
 	}
 	
-	public Piece piece(Position position) {		
+	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição não existe no tabuleiro");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
+	public void placepiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("Já existe uma peça nesta posição.");
+		}
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
+	
+	private boolean positionExists(int row, int column) {		
+		return row >=0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição não existe no tabuleiro");
+		}
+		return piece(position) != null;
+	}
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -8,6 +11,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8,8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -18,6 +22,17 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placepiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
+	private void initialSetup() {
+		placeNewPiece('b', 6, new King(board, Color.WHITE));
+		placeNewPiece('f', 5, new Rook(board, Color.WHITE));
+		
+		
 	}
 
 }
